3f649a8c3cf279f5cc681cb0f473aec56e018dda
BAEK GYEONGCHEOL <bkctwo@tukorea.ac.kr>
2025-11-03 22:20:41 +0900

    bird 비둘기(빨간 비둘기???)에 맞게 수정, 약 21cm, 시속 70km, 날개짓 속도 5회!

--- bird.py
+++ bird.py
@@ -7,16 +7,20 @@ sprites = (
     (0, 0), (50, 0), (100, 0), (150, 0),
 )
 
+# 빨간 비둘기???
+# 새의 크기: 약 21cm(공백 제외 20픽셀)
+# 새의 속도: 시속 70km/h
+# 날개짓 속도: 초당 5회(총 14개의 이미지)
+
 PIXEL_PER_METER = (10.0 / 0.3)  # 10 pixel 30 cm
-RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_KMPH = 70.0  # Km / Hour
 RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
 RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
 RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
 
-# Boy Action Speed
-TIME_PER_ACTION = 1 / 10.0  # 한 번 푸드덕 하는데 걸리는 시간
-ACTION_PER_TIME = 1.0 / TIME_PER_ACTION # 초당 푸드덕 15회!
-FRAMES_PER_ACTION = 10
+TIME_PER_ACTION = 1 / 5.0  # 한 번 푸드덕 하는데 걸리는 시간
+ACTION_PER_TIME = 1.0 / TIME_PER_ACTION # 초당 푸드덕 5회!
+FRAMES_PER_ACTION = 14
 
 class Bird:
     image = None
@@ -36,7 +40,7 @@ class Bird:
             self.image.clip_composite_draw(frame_x, frame_y, 50, 46, 0, 'h', self.x, self.y, 25, 23)
 
     def update(self):
-        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 10
+        self.frame = (self.frame + FRAMES_PER_ACTION * ACTION_PER_TIME * game_framework.frame_time) % 14
         self.x += self.face_dir * RUN_SPEED_PPS * game_framework.frame_time
 
         if self.x < 25:
